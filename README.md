332d972ae4541cdcf0d0c8a13eb33913b06ee2af
zd-demo <2304138065@qq.com>
2020-02-17 09:59:41 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## community
 
 ## 资料
-https://spring.io/guides/gs/
-https://spring.io/guides/gs/serving-web-content/
-https://elasticsearch.cn/explore
+[Spring 文档](https://spring.io/guides/gs/)
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)
+[es](https://elasticsearch.cn/explore)
+[BootStrap](https://v3.bootcss.com/getting-started/)
+[Github Oauth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 
 ## 工具
-https://git-scm.com/downloads
+[Git](https://git-scm.com/downloads)
---
+++ src/main/java/com/zd/community/dto/GithubUser.java
@@ -0,0 +1,37 @@
+package com.zd.community.dto;
+
+/**
+ * @author 张东
+ * @create 2020-02-16 21:17
+ * @desc
+ */
+public class GithubUser {
+
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
